c9ecb46bfb55419dbf6ec9500d4305e8f88cea6d
Camille Onoda <info@camilleonoda.com>
2023-12-05 12:02:59 +0900

    Declare variables with let and const, display a message with alert()

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Hello Camille!")
+
+let message = "How are you today?";
+//alert(message);
+
+let hello = "Hello world!";
+let msg = hello;
+//alert(hello);
+//alert(msg);
+
+const birthday = "15/09/1988";
+console.log(birthday)
+
+const COLOR_RED = "#F00"; //capital-named constants are only used as aliases for “hard-coded” values
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_GREEN;
+//alert(color);
