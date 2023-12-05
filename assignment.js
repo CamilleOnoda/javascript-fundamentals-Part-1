f757e04547a0be5c4d6f7d1cea103c51f91f02f3
Camille Onoda <info@camilleonoda.com>
2023-12-05 14:42:17 +0900

    Practice declaration with let and const and learn the equality operator and the strict equality operator

---
+++ assignment.js
@@ -0,0 +1,20 @@
+console.log(23 + 97)
+console.log(12 + 23 + 96 + 54 + 78 + 54)
+let  num = (4 + 6 + 9) / 77;
+console.log(num)
+
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log(percentage)
+
+//Equality operator
+1 == 1; // true
+1 == '1'; // true
+1 == 2; // false
+
+//Strict equality operator
+//It requires both the value and the type to be the same for the comparison to be true.
+1 === '1'; // false
+1 === 1; // true
+1 === 2; // false
